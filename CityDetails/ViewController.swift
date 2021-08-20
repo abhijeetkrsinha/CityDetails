8feb573fb6dcf8e9400c1a032d73559e2da1b9e1
ABHIJEET <abhijeet.sinha38@gmail.com>
2021-08-20 13:35:07 +0530

    Commit changes in city details

--- CityDetails/ViewController.swift
+++ CityDetails/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
-
+import GooglePlaces
 class ViewController: UIViewController {
+    private var placesClient: GMSPlacesClient!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        placesClient = GMSPlacesClient.shared()
+
         // Do any additional setuphi check after loading the view.
     }
 
+    
+    // Add a UIButton in Interface Builder, and connect the action to this function.
+     @IBAction func getCurrentPlace(_ sender: UIButton) {
+        let fields: GMSPlaceField = GMSPlaceField(rawValue:
+          UInt(GMSPlaceField.name.rawValue) | UInt(GMSPlaceField.placeID.rawValue))!
+
+        placesClient.findPlaceLikelihoodsFromCurrentLocation(withPlaceFields: fields) { [weak self] (placeLikelihoods, error) in
+         guard let strongSelf = self else {
+           return
+         }
+
+         guard error == nil else {
+           print("Current place error: \(error?.localizedDescription ?? "")")
+           return
+         }
+
+         guard let place = placeLikelihoods?.first?.place else {
+           return
+         }
+
+       }
+     }
 
 }
 
